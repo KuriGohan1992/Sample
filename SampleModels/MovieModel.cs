d0dda2e2a42eddf210c653eab6afcb97cced4e05
chamm <chamm@DESKTOP-QH2E47P>
2024-10-19 01:48:55 +0800

    Test

---
+++ SampleModels/MovieModel.cs
@@ -0,0 +1,12 @@
+﻿namespace SampleModels
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public float Rating { get; set; }
+        public int Year_Released { get; set; }
+        public string Director {  get; set; }
+
+    }
+}
